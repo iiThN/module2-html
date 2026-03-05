609549b058609a8234a4a674a8db1184e6e613e7
Anthony Capuras <capuras.anthony2003@gmail.com>
2026-03-05 15:35:14 +0800

    WSAT-M6: Enhanced React CV with props and dynamic rendering

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -5,19 +5,15 @@ return (
     <div className= "container">
         <section class="card">
             <Card title="About Me">
-                <p>This is my personal profile.</p>
-            </Card>
-            );
-
             <img src={profile} alt="Profile" className="about-img" />
             <p>I’m a second-year IT student who enjoys quiet moments, good music, and a warm cup of coffee.</p>
-
             <p>
                 Email:
                 <a href="mailto:capuras.anthony2003@gmail.com">capuras.anthony2003@gmail.com</a>
                 GitHub:
                 <a href="https://github.com" target="_blank">github.com</a>
             </p>
+            </Card>
         </section>
     </div>
 );
