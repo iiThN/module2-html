9af66da42fa8d945a83abe54336cd671e5efcdd5
Anthony Capuras <capuras.anthony2003@gmail.com>
2026-04-22 23:17:50 +0800

    Added email and message

--- personal-cv-react/src/components/ContactList.jsx
+++ personal-cv-react/src/components/ContactList.jsx
@@ -2,22 +2,45 @@ import { useEffect, useState } from "react";
 
 function ContactList() {
     const [contacts, setContacts] = useState([]);
+    const [visible, setVisible] = useState(true);
+
     useEffect(() => {
         fetch("http://localhost/cv-api/getContacts.php")
             .then(res => res.json())
             .then(data => setContacts(data));
     }, []);
+
     return (
-    <div className="container">
-        <section className="card">
-            <h2>Saved Contacts</h2>
-            <ul>
-                {contacts.map(contact => (
-                    <li key={contact.id}>{contact.name}</li>
-                ))}
-            </ul>
+        <div className="container">
+            <section className="card">
+                <button onClick={() => setVisible(!visible)}>
+                    {visible ? "Hide Saved Contacts" : "Show Saved Contacts"}
+                </button>
+
+                <h2>Saved Contacts</h2>
+                {visible && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contacts.map((contact) => (
+                            <tr key={contact.id}>
+                                <td>{contact.name}</td>
+                                <td>{contact.email}</td>
+                                <td>{contact.message}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                )}
             </section>
-    </div>
+        </div>
     );
 }
+
 export default ContactList;
